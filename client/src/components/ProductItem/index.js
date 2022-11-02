b2aba3c8294e2dbb7d374f8a2b08c3d636336316
Claire Cashmore <clairecashmore2017@gmail.com>
2022-11-02 12:24:27 -0400

    mobile view altered

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { idbPromise, pluralize } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
 import Auth from "../../utils/auth.js";
+import SignupForm from "../../pages/SignupForm";
+import LoginForm from "../../pages/LoginForm";
 function ProductItem(item) {
   // define our state variable
   const [state, dispatch] = useStoreContext();
   const { cart } = state;
   const loggedIn = Auth.loggedIn();
+  const [showModal, setShowModal] = useState(false);
   const addToCart = () => {
     // find the cart item with the matching id
     const itemInCart = cart.find((cartItem) => cartItem._id === _id);
@@ -35,27 +39,63 @@ function ProductItem(item) {
   const { image, name, _id, price, quantity } = item;
 
   return (
-    <div className="card px-1 py-1">
-      <Link to={`/products/${_id}`}>
-        <img className="prod-img" alt={name} src={`/images/${image}`} />
-        <p className="mini-title abril">{name}</p>
-      </Link>
-      <div>
-        {/* <div>
+    <>
+      <div className="card px-1 py-1">
+        <Link to={`/products/${_id}`}>
+          <img className="prod-img" alt={name} src={`/images/${image}`} />
+          <p className="mini-title abril">{name}</p>
+        </Link>
+        <div>
+          {/* <div>
           {quantity} {pluralize("item", quantity)} in stock
         </div> */}
-        <span>${price}</span>
+          <span>${price}</span>
+        </div>
+        {loggedIn ? (
+          <button className="cart-btn" onClick={addToCart}>
+            Add to cart
+          </button>
+        ) : (
+          <button onClick={() => setShowModal(true)}>Add to cart</button>
+        )}
       </div>
-      {loggedIn ? (
-        <button className="cart-btn" onClick={addToCart}>
-          Add to cart
-        </button>
-      ) : (
-        <button disabled={true} onClick={addToCart}>
-          Login to purchase
-        </button>
-      )}
-    </div>
+      <Modal
+        size="lg"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey="login">
+          <Modal.Header closeButton>
+            <Modal.Title id="signup-modal">
+              <Nav variant="pills">
+                <Nav.Item>
+                  <Nav.Link eventKey="login">
+                    <p className="login">Login</p>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="signup">
+                    <p className="login">Sign Up</p>
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey="login">
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="signup">
+                <SignupForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+    </>
   );
 }
 
